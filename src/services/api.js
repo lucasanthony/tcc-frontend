fc54f708e79aaf7a7fea79bcdc43191c53760ebb
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2022-01-17 13:30:30 -0300

    Alterando url da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:4444/',
+    baseURL: 'https://reportappcodex.herokuapp.com/',
     headers: {
         "Content-type": "application/json"
     }
