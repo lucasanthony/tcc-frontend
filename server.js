2f97802b24ab8b6ba45b830afab5953122b47fd4
lucasanthony <lucas.anthony.oliveira@ccc.ufcg.edu.br>
2022-06-17 09:01:56 -0300

    ajuste para deploy

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback');
 
-var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+const app = express();
 
-var port = process.env.PORT || 5000
-app.listen(port)
-console.log('server started ' + port)
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+
+app.use(staticFileMiddleware);
+app.use(history({
+    disableDotRule: true,
+    verbose: true
+}));
+app.use(staticFileMiddleware);
+
+app.get('/', function (req, res) {
+    res.render(path.join(__dirname + '/dist/index.html'));
+});
+
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
