a426f8c16d55af4286cb721010c3e93bd98b0731
FLuiz22 <filipeluiz39@gmail.com>
2023-09-13 21:51:59 -0300

    Feat: AdicionarAtualizacao modal created

---
+++ src/components/modals/AdicionarAtualizacao.vue
@@ -0,0 +1,82 @@
+<template lang="pug">
+div.modal-content
+    div.col
+        el-divider(
+            content-position="left"
+        ) Descrição
+        el-input(
+            v-model="news.description"
+            type="textarea"
+            placeholder="Descrição"
+            :disabled="isVisualizar"
+        )
+        el-divider(
+            content-position="left"
+        ) URL
+        el-input(
+            placeholder="URl"
+            v-model="news.url"
+            :disabled="isVisualizar"
+        )
+        el-divider(
+            content-position="left"
+        ) Imagem
+        el-input(
+            v-model="news.image"
+            type="file"
+            accept="image/*"
+            @change="onImageSelected"
+            :disabled="isVisualizar"
+        )
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+    name: 'AdicionarAtualizacao',
+
+    props: {
+        news: {
+            type: Object,
+            required: true
+        },
+        isVisualizar: {
+            type: Boolean,
+            required: false,
+            default: false
+        }
+    },
+
+    data() {
+        return {
+            dados:[]
+        }
+    },
+
+    methods: {
+        ...mapActions({
+            findById: 'findById'
+        }),
+
+        onImageSelected(event) {
+            console.log(event);
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.modal-content {
+  display: flex;
+  gap: 2%;
+  justify-content: center;
+}
+
+.col {
+  display: flex;
+  flex-direction: column;
+  gap: 2vh;
+  flex: 1;
+}
+</style>
