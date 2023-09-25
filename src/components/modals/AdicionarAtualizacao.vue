03d40a099a65de7dfb73aa4c849ac5458bc80dd3
ManoMax <gabriel.matos@ccc.ufcg.edu.br>
2023-09-25 17:24:36 -0300

    style: image input removed and URL string changed

--- src/components/modals/AdicionarAtualizacao.vue
+++ src/components/modals/AdicionarAtualizacao.vue
@@ -1,91 +1,91 @@
 <template lang="pug">
 div.modal-content
-    div.col
-        el-divider(
-            content-position="left"
-        ) Descrição
-        el-input(
-            v-model="atualizacao.description"
-            type="textarea"
-            placeholder="Descrição"
-            :disabled="isVisualizar"
-        )
-        el-divider(
-            content-position="left"
-        ) URL
-        el-input(
-            placeholder="URl"
-            v-model="atualizacao.updateLink"
-            :disabled="isVisualizar"
-        )
-        el-divider(
-            content-position="left"
-        ) Imagem
-        input(
-            type="file"
-            ref="imageInput"
-            accept="image/*"
-            @change="onImageSelected"
-            :disabled="isVisualizar"
-        )
+   div.col
+      el-divider(
+         content-position="left"
+      ) Descrição
+      el-input(
+         v-model="atualizacao.description"
+         type="textarea"
+         placeholder="Descrição"
+         :disabled="isVisualizar"
+      )
+      el-divider(
+         content-position="left"
+      ) URL
+      el-input(
+         placeholder="URL"
+         v-model="atualizacao.updateLink"
+         :disabled="isVisualizar"
+      )
+      // el-divider(
+      //    content-position="left"
+      // ) Imagem
+      // input(
+      //    type="file"
+      //    ref="imageInput"
+      //    accept="image/*"
+      //    @change="onImageSelected"
+      //    :disabled="isVisualizar"
+      // )
 </template>
 
 <script>
 import { mapActions } from 'vuex';
 
 export default {
-    name: 'AdicionarAtualizacao',
+   name: 'AdicionarAtualizacao',
 
-    props: {
-        atualizacao: {
-            type: Object,
-            required: true
-        },
-        isVisualizar: {
-            type: Boolean,
-            required: false,
-            default: false
-        }
-    },
+   props: {
+      atualizacao: {
+         type: Object,
+         required: true
+      },
+      isVisualizar: {
+         type: Boolean,
+         required: false,
+         default: false
+      }
+   },
 
-    data() {
-        return {
-            dados:[],
-        }
-    },
+   data() {
+      return {
+         dados: [],
+      }
+   },
 
-    methods: {
-        ...mapActions({
-            findById: 'findById'
-        }),
+   methods: {
+      ...mapActions({
+         findById: 'findById'
+      }),
 
-        onImageSelected(e) {
-            this.atualizacao.image = e.target.files[0]
-            this.convertImageToBuffer();
-        },
+      onImageSelected(e) {
+         this.atualizacao.image = e.target.files[0]
+         this.convertImageToBuffer();
+      },
 
-        async convertImageToBuffer() {
-            if(!this.atualizacao.image) {
-                return;
-            }
-            
-            this.atualizacao.image = new Uint8Array(await this.atualizacao.image.arrayBuffer());
-        }
-    }
+      async convertImageToBuffer() {
+         if (!this.atualizacao.image) {
+            return;
+         }
+
+         this.atualizacao.image = new Uint8Array(await this.atualizacao.image.arrayBuffer());
+      }
+   }
 }
 </script>
 
 <style lang="scss" scoped>
 .modal-content {
-  display: flex;
-  gap: 2%;
-  justify-content: center;
+   display: flex;
+   gap: 2%;
+   justify-content: center;
 }
 
 .col {
-  display: flex;
-  flex-direction: column;
-  gap: 2vh;
-  flex: 1;
+   display: flex;
+   flex-direction: column;
+   gap: 2vh;
+   flex: 1;
 }
 </style>
