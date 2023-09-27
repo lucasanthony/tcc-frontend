ae6edd8c29f11a8d7413c89fda03ac5286ef60bb
ManoMax <gabriel.matos@ccc.ufcg.edu.br>
2023-09-27 15:04:50 -0300

    refactor: try catch added and req (params and body) updated

--- src/store/modules/news.js
+++ src/store/modules/news.js
@@ -1,37 +1,108 @@
 import axios from '@/utils/axios'
 
 const actions = {
+
     async findAllNews({ commit }) {
-        const result = await axios({
+      try {
+         const result = await axios({
             method: 'GET',
             url: 'news'
         })
         return result.data
+       } catch (err) {
+         const errorMessage = err.response.data.error;
+   
+         isBadToken(errorMessage)
+           ? console.log(err.response.data.error) || localStorage.clear()
+           : null; // To inspect, you can change null by console.log(res)
+   
+         return err.response;
+       }
     },
+
+    async getNewsByProject({ commit }, data) {
+      try {
+         const result = await axios({
+            method: 'GET',
+            url: `news/${data.projectId}`
+         })
+         return result.data
+       } catch (err) {
+         const errorMessage = err.response.data.error;
+   
+         isBadToken(errorMessage)
+           ? console.log(err.response.data.error) || localStorage.clear()
+           : null; // To inspect, you can change null by console.log(res)
+   
+         return err.response;
+       }
+    },
+
     async createNews({ commit }, dados) {
-        const result = await axios({
+      try {
+         const result = await axios({
             method: 'POST',
             url: `news/${dados.projectId}`,
             data: dados.news
-        })
-        return result.data
+         })
+         return result.data
+       } catch (err) {
+         const errorMessage = err.response.data.error;
+   
+         isBadToken(errorMessage)
+           ? console.log(err.response.data.error) || localStorage.clear()
+           : null; // To inspect, you can change null by console.log(res)
+   
+         return err.response;
+       }
     },
+
     async updateNews({ commit }, dados) {
-        const result = await axios({
+      try {
+         const result = await axios({
             method: 'PATCH',
-            url: `news/${dados.id_project}`,
-            data: dados.news
-        })
-        return result.data
+            url: `news/${dados._id}`,
+            data: { news: dados }
+         });
+         return result.data;
+       } catch (err) {
+         const errorMessage = err.response.data.error;
+   
+         isBadToken(errorMessage)
+           ? console.log(err.response.data.error) || localStorage.clear()
+           : null; // To inspect, you can change null by console.log(res)
+   
+         return err.response;
+       }
     },
-    async deleteNews({ commit }, id) {
-        const result = await axios({
+
+    async deleteNews({ commit }, dados) {
+      try {
+         const result = await axios({
             method: 'DELETE',
-            url: `news/${id}`
-        })
-        return result.data
+            url: `news/${dados.projectId}`,
+            data: { newsId: dados.newsId }
+         });
+         return result.data;
+       } catch (err) {
+         const errorMessage = err.response.data.error;
+   
+         isBadToken(errorMessage)
+           ? console.log(err.response.data.error) || localStorage.clear()
+           : null; // To inspect, you can change null by console.log(res)
+   
+         return err.response;
+       }
     }
 }
+
+function isBadToken(errorMessage) {
+   return [
+     "Token inválido.",
+     "Usuário não existe.",
+     "Token mal formatado.",
+   ].includes(errorMessage);
+ }
 
 export default {
     actions
